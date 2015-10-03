e76822601bb1075b60ee92074a75a698e6cc7722
Segundo Leon <segundoleon@msn.com>
2015-10-03 18:17:34 +0000

    al 13:17

--- Proyecto_Final/taxixpress/app/models/cost.rb
+++
@@ -1,4 +0,0 @@
-class Cost < ActiveRecord::Base
-  belongs_to :from_distrit, class_name: 'Distrit'
-  belongs_to :to_distrit, class_name: 'Distrit'
-end
--- Proyecto_Final/taxixpress/app/views/costs/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @cost, :id, :estimated_cost, :from_distrit_id, :to_distrit_id, :created_at, :updated_at
