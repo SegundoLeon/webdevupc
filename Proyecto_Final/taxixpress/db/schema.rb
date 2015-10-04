380187b81158734f327ec401e293cf8503aee9cb
rogereduard <paucar.roger@gmail.com>
2015-10-04 15:03:26 +0000

    a

--- Proyecto_Final/taxixpress/db/schema.rb
+++ Proyecto_Final/taxixpress/db/schema.rb
@@ -11,35 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151004012924) do
+ActiveRecord::Schema.define(version: 20151001025402) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "name",        limit: 255
     t.string   "reference",   limit: 255
     t.string   "alias",       limit: 255
     t.boolean  "favourite",   limit: 1
-    t.integer  "profile_id",  limit: 4
+    t.integer  "user_id",     limit: 4
     t.integer  "district_id", limit: 4
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
   end
 
   add_index "addresses", ["district_id"], name: "index_addresses_on_district_id", using: :btree
-  add_index "addresses", ["profile_id"], name: "index_addresses_on_profile_id", using: :btree
-
-  create_table "costs", force: :cascade do |t|
-    t.integer  "estimated_cost",   limit: 4
-    t.integer  "from_district_id", limit: 4
-    t.integer  "to_district_id",   limit: 4
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-  end
-
-  create_table "districts", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-  end
+  add_index "addresses", ["user_id"], name: "index_addresses_on_user_id", using: :btree
 
   create_table "payment_methods", force: :cascade do |t|
     t.string   "name",        limit: 255
@@ -66,33 +52,6 @@ ActiveRecord::Schema.define(version: 20151004012924) do
     t.datetime "updated_at",                null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255,                default: "",    null: false
-    t.string   "encrypted_password",     limit: 255,                default: "",    null: false
-    t.string   "reset_password_token",   limit: 255
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,                  default: 0,     null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.datetime "created_at",                                                        null: false
-    t.datetime "updated_at",                                                        null: false
-    t.string   "name",                   limit: 255
-    t.string   "surname",                limit: 255
-    t.decimal  "mobilephone",                        precision: 10
-    t.decimal  "phone",                              precision: 10
-    t.datetime "date_of_birth"
-    t.boolean  "is_female",              limit: 1,                  default: false
-    t.string   "address",                limit: 255
-    t.string   "marital_status",         limit: 255
-    t.string   "company",                limit: 255
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
   create_table "vehicle_types", force: :cascade do |t|
     t.string   "name",        limit: 255
     t.text     "description", limit: 65535
@@ -114,8 +73,6 @@ ActiveRecord::Schema.define(version: 20151004012924) do
   add_index "vehicles", ["profile_id"], name: "index_vehicles_on_profile_id", using: :btree
   add_index "vehicles", ["vehicle_type_id"], name: "index_vehicles_on_vehicle_type_id", using: :btree
 
-  add_foreign_key "addresses", "districts"
-  add_foreign_key "addresses", "profiles"
   add_foreign_key "profiles", "user_types"
   add_foreign_key "vehicles", "profiles"
   add_foreign_key "vehicles", "vehicle_types"
