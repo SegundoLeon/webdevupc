42b0d2f68bb2a91d6813aa3bbee9fa1922cb362e
rogereduard <paucar.roger@gmail.com>
2015-10-09 23:08:48 +0000

    paginacion

--- Proyecto_Final/taxixpress/app/controllers/services_controller.rb
+++ Proyecto_Final/taxixpress/app/controllers/services_controller.rb
@@ -12,9 +12,9 @@ class ServicesController < ApplicationController
       when "Cliente"
         @services = Service.where(user_id: current_user.id).order(created_at: :desc).limit(10)
       when "Operador"
-        @services = Service.all
+        @services = Service.all.paginate(page: params[:page], per_page: 5)
       when "Taxista"
-        @services = Service.all
+        @services = Service.all.paginate(page: params[:page], per_page: 5)
         #@services = Service.where(vehicle_id: current_user.vehicle.user_id)
     end
   end
@@ -47,6 +47,7 @@ class ServicesController < ApplicationController
         format.json { render json: @service.errors, status: :unprocessable_entity }
       end
     end
+    
   end
 
   # PATCH/PUT /services/1
@@ -81,6 +82,6 @@ class ServicesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def service_params
-      params.require(:service).permit(:user_id, :from_address_id, :to_address_id, :date_time, :payment, :passengers, :vehicle_type_id, :payment_method_id, :service_code, :vehicle_id, :rate, :experience, :status)
+      params.require(:service).permit(:user_id, :from_address_id, :to_address_id, :date_time, :payment, :passengers, :vehicle_type_id, :payment_method_id, :service_code, :rate, :experience, :status)
     end
 end
--- Proyecto_Final/taxixpress/app/models/service.rb
+++ Proyecto_Final/taxixpress/app/models/service.rb
@@ -5,12 +5,30 @@ class Service < ActiveRecord::Base
   belongs_to :vehicle_type
   belongs_to :payment_method
   belongs_to :vehicle
-  validates :from_district, presence:true
-  validates :to_district, presence:true
   validates :passengers, presence:true
   validates :payment, presence:true
   validates :vehicle_type, presence:true
   validates :payment_method, presence:true
-  validates :vehicle, presence:true
   validates :rate, presence:true
+  
+  def user_name
+      self.user.name
+  end
+  
+  def payment_method_name
+      self.payment_method.name
+  end
+  
+  def vehicle_type_name
+      self.vehicle_type.name
+  end
+  
+  def from_address_name
+      self.from_address.name
+  end
+  
+  def to_address_name
+      self.to_address.name
+  end
+  
 end
